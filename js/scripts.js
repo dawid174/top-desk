c6666d0d4ff3e0f7bd093891804cff0db2f63c84
Dawid Jarosiński <dawid174@gmail.com>
2016-07-04 10:57:27 +0200

    files load
    first files load

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+window.onload = init_JS;
+
+var days = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"];
+var months = ["stycznia", "lutego", "marca", "kwietnia", "maja", "czerwca", "lipca", "sierpnia", "września", "października", "listopada", "grudnia"];
+var weatherArray = ["na", "wind", "wind", "cloud-flash", "cloud-flash", "snow", "rain", "snow", "drizzle", "drizzle",
+					"windy-rain", "drizzle", "drizzle", "snow-heavy", "snow", "snow-heavy", "snow", "hail", "snow", "wind",
+					"fog", "mist", "mist", "mist", "wind", "temperature", "cloud", "cloud-moon", "clouds", "cloud-moon",
+					"cloud", "moon", "sun", "moon", "sun", "rain", "sun", "cloud-flash", "cloud-flash", "cloud-flash",
+					"hail", "snow-heavy", "snow", "snow-heavy", "cloud", "cloud-flash", "snow", "cloud-flash", "na"];
+
+function init_JS()
+{
+	setClock();
+	getWeather();
+}
+
+function setClock()
+{
+	var date = new Date();
+	d = date.getDay();		// dzień tygodnia (0-6)
+	D = date.getDate();		// dzień miesiąca (1-31)
+	M = date.getMonth();	// miesiąc (0-11)
+	Y = date.getFullYear();	// rok (4 cyfr)
+ 
+	h = date.getHours();	// godzina (24h)
+	m = date.getMinutes();	// minuta (0-59)
+	s = date.getSeconds();	// sekunda (0-59)
+
+	if(m < 10) m = "0" + m;
+	if(s < 10) s = "0" + s;
+
+	document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+	document.getElementById("date").innerHTML = days[d] + ", " + D + " " + months[M];
+	setTimeout("setClock()",1000);
+}
+
+function getWeather()
+{
+	var xmlhttp = new XMLHttpRequest();
+	var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22legnica%2C%20poland%22)%20and%20u%3D'c'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+	xmlhttp.onreadystatechange = function() {
+	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var json = JSON.parse(xmlhttp.responseText);
+			var wTemp = parseFloat(json.query.results.channel.item.condition.temp);
+			var wCode = parseFloat(json.query.results.channel.item.condition.code);
+			
+			document.getElementById("weather_temp").innerHTML = wTemp + "°";
+			document.getElementById("weather_icon").innerHTML = '<i class="icon-' + weatherArray[wCode] + '"></i>';
+		}
+	};
+
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
